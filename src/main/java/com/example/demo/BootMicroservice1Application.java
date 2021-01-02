7acd50e0d23869c32cc0b316e6be97000f81901f
pratiksoni <pratiksonii@yahoo.in>
2021-01-02 15:19:21 +0530

    new endpoint

--- src/main/java/com/example/demo/BootMicroservice1Application.java
+++ src/main/java/com/example/demo/BootMicroservice1Application.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class BootMicroservice1Application {
+	
+	@Value( "${name}" )
+	private String name;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootMicroservice1Application.class, args);
@@ -17,5 +21,11 @@ public class BootMicroservice1Application {
 	public String getHello() {
 		return "from Microservice 1";
 	}
+	
+	@GetMapping("/name")
+	public String getName() {
+		return "my name is " + name;
+	}
+	
 
 }
